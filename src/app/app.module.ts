68c06646eca724ba6345c6ae0535572b5453ca4b
Daniela Correa <dacomu24@gmail.com>
2024-09-28 17:20:47 -0500

    Folder structure, task and create task components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ListComponent } from './domains/tasks/pages/list/list.component';
+import { TaskComponent } from './domains/tasks/components/task/task.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListComponent
+    ListComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
--- src/app/domains/tasks/pages/list/list.component.ts
+++ src/app/domains/tasks/pages/list/list.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TaskComponent } from '../../components/task/task.component';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.css']
+  styleUrls: ['./list.component.css'],
 })
 export class ListComponent {
 
