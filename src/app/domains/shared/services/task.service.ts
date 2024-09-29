5b4e05a3304b76fa0f7a6563acc0386d7c933830
Daniela Correa <dacomu24@gmail.com>
2024-09-28 23:06:00 -0500

    Get and create task in service, create task reactive form group

--- src/app/domains/shared/services/task.service.ts
+++ src/app/domains/shared/services/task.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../models/task.model';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
+  private tasks: Task[] = [];
+
+  private tasksSubject: BehaviorSubject<Task[]> = new BehaviorSubject(this.tasks);
+
+
   constructor() { }
+
+  // ngOnInit() {
+  //   const storage = localStorage.getItem('tasks');
+  //   if (storage) {
+  //     const tasks = JSON.parse(storage);
+  //     this.tasks = [...this.tasks, tasks];
+  //   }
+  // }
+
+
+
+  getTasks(): Observable<Task[]> {
+    return this.tasksSubject.asObservable();
+  }
+
+  addTask(newTask: Task): void {
+    this.tasks.push(newTask);
+    this.tasksSubject.next(this.tasks);
+    console.log("entre", this.tasks);
+
+  }
 }
--- src/app/domains/tasks/components/create-task/create-task.component.ts
+++ src/app/domains/tasks/components/create-task/create-task.component.ts
@@ -1,5 +1,10 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TaskService } from 'src/app/domains/shared/services/task.service';
+import { Task } from 'src/app/domains/shared/models/task.model';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-create-task',
@@ -9,6 +14,7 @@ import { CommonModule } from '@angular/common';
   styleUrls: ['./create-task.component.css']
 })
 export class CreateTaskComponent {
+  createTaskForm!: FormGroup;
 
   @Output() closeCreateTask = new EventEmitter();
 
@@ -16,5 +22,31 @@ export class CreateTaskComponent {
     this.closeCreateTask.emit();
   }
 
+  constructor(
+    private taskService: TaskService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit():void {
+    this.createTaskForm = this.formBuilder.group({
+      taskName: ['',Validators.required],
+      deadlineDate: ['',Validators.required],
+      personName: ['',Validators.required],
+      age: ['',Validators.required],
+
+
+    })
+  }
+
+  addTask(): void {
+    const newTask: Task = {
+      id: Math.floor(Math.random() * 1000),
+      title: 'New Task',
+      deadlineDate: '2024-10-10',
+      associatedPeople: [],
+      completed: false
+    };
+    this.taskService.addTask(newTask);
+  }
 
 }
